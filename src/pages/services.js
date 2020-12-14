f9fba1e6b2ccac5dd52cf6f05ad7fa1b697e0281
stevewil <stevewil@gmail.com>
2020-12-14 14:59:46 -0800

    auth commit 1

---
+++ src/pages/services.js
@@ -0,0 +1,19 @@
+//create src/pages/services.js
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+//import SEO from "../components/seo";
+
+//import { withAuthenticator } from "aws-amplify-react"
+
+const Services = () => (
+    <Layout>
+        
+        <h1>Services</h1>
+    </Layout>
+)
+
+//export default withAuthenticator(Services)
+export default Services
+//end
