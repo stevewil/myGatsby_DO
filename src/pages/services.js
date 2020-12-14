3ed3cbf3b07780ae4b00049fa3042f6605d4be37
stevewil <stevewil@gmail.com>
2020-12-14 15:18:34 -0800

    aws-amplify with cognito is working with Google social identity

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,19 +1,48 @@
-//create src/pages/services.js
-import React from "react";
-import { Link } from "gatsby";
+import React, { useEffect, useState } from 'react';
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import awsconfig from '../aws-exports';
 
-import Layout from "../components/layout";
-//import SEO from "../components/seo";
+Amplify.configure(awsconfig);
 
-//import { withAuthenticator } from "aws-amplify-react"
+function App() {
+  const [user, setUser] = useState(null);
 
-const Services = () => (
-    <Layout>
-        
-        <h1>Services</h1>
-    </Layout>
-)
+  useEffect(() => {
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+        case 'cognitoHostedUI':
+          getUser().then(userData => setUser(userData));
+          break;
+        case 'signOut':
+          setUser(null);
+          break;
+        case 'signIn_failure':
+        case 'cognitoHostedUI_failure':
+          console.log('Sign in failure', data);
+          break;
+      }
+    });
 
-//export default withAuthenticator(Services)
-export default Services
-//end
+    getUser().then(userData => setUser(userData));
+  }, []);
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then(userData => userData)
+      .catch(() => console.log('Not signed in'));
+  }
+
+  return (
+    <div>
+      <p>User: {user ? JSON.stringify(user.attributes) : 'None'}</p>
+      {user ? (
+        <button onClick={() => Auth.signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => Auth.federatedSignIn()}>Federated Sign In</button>
+      )}
+    </div>
+  );
+}
+
+export default App;
